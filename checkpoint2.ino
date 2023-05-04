d7d1baa55b0003bc9c9b12f40fb62c4c6c83aea0
Henrique Pontes <126187491+Pontessxx@users.noreply.github.com>
2023-05-04 16:17:46 -0300

    Codigo final

--- checkpoint2.ino
+++ checkpoint2.ino
@@ -4,17 +4,17 @@ int YELLOW_LED_PIN = 3;  // pino digital do led amarelo
 int RED_LED_PIN = 4;     // pino digital do led vermelho
 int BUZZER_PIN = 5;      // pino digital do buzzer
 
-int lightValue = 0;    // variável para armazenar a leitura do LDR
+int lightValue = 0;    // variavel para armazenar a leitura do LDR
 int greenState = LOW;  // estado do led verde
 int yellowState = LOW; // estado do led amarelo
 int redState = LOW;    // estado do led vermelho
 
-const int LDR_PIN = A0; // variável para armazenar o valor lido pelo ADC
+const int LDR_PIN = A0; // variavel para armazenar o valor lido pelo ADC
 const int sensorPin = A5;
 float valorSensor;
 float temperaturaC, temperaturaF;
-float tensao = 0.0;    // variável para armazenar a tensão
-
+float tensao = 0.0;    // variavel para armazenar a tensao
+float tensao_ms = 0.0; 
 LiquidCrystal lcd(12,11,9,8,7,6);
 void setup() {
   Serial.begin(9600);
@@ -26,74 +26,82 @@ void setup() {
   pinMode(sensorPin, INPUT);
   lcd.begin(16, 2);
 }
-
 void loop() {
   // Leitura do valor do LDR
   lightValue = analogRead(LDR_PIN);
   valorSensor = analogRead(sensorPin);
-  tensao = valorSensor * 5.0 / 1024.0; // encontra a tensão do sensor
+  tensao = valorSensor * 5.0 / 1024.0; // encontra a tensao do sensor
   temperaturaC = ((tensao - 0.5) * 100.0); // converte para temperatura em graus Celsius
-  
-  // Checagem do nível de luminosidade
-  if (lightValue <= 935 && lightValue>=755) {
-    // Luminosidade média - liga led amarelo e desliga verde
+
+  if ((lightValue <= 935 && lightValue>=755) && (temperaturaC>=16 && temperaturaC<=25)) {
+    // Luminosidade media - liga led amarelo e desliga verde
     digitalWrite(GREEN_LED_PIN, LOW);
     digitalWrite(YELLOW_LED_PIN, HIGH);
     digitalWrite(RED_LED_PIN, LOW);
-    noTone(BUZZER_PIN);   // para o som do buzzer
-    
-    //lcd.clear();
-    //lcd.setCursor(0, 0);
-    //lcd.print("LUZ e TEMP:");
-    //lcd.setCursor(0, 1);
-    //lcd.print("MEDIA");
-    //delay(2000);
-    
-  } else if (lightValue >= 935) {
+    noTone(BUZZER_PIN); // para o som do buzzer
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("LUZ: MEDIA");
+    lcd.setCursor(0, 1);
+    lcd.print("TEMP: MEDIA");
+    delay(2000);
+} else if ((lightValue >= 935) || (temperaturaC<=0 ||temperaturaC>=26)) {
     // Luminosidade alta - liga led vermelho e desliga verde e amarelo
     digitalWrite(GREEN_LED_PIN, LOW);
     digitalWrite(YELLOW_LED_PIN, LOW);
     digitalWrite(RED_LED_PIN, HIGH);
     tone(BUZZER_PIN, 1000);// toca um som de 1kHz no buzzer
-    	delay(1000);
+    delay(1000);
     tone(BUZZER_PIN, 2000);
     noTone(BUZZER_PIN);
-    
-    //lcd.clear();
-    //lcd.setCursor(0, 0);
-    //lcd.print("CUIDADO!!!");
-    //lcd.setCursor(0, 1);
-    //lcd.print("Produto danificado");
-    //delay(2000);
-    
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("ALERTA!!!");
+    lcd.setCursor(0, 1);
+    lcd.print("Condicoes ruins");
+    delay(2000);
+	}
+else{
+  // Luminosidade baixa - liga led verde e desliga os outros
+  digitalWrite(GREEN_LED_PIN, HIGH);
+  digitalWrite(YELLOW_LED_PIN, LOW);
+  digitalWrite(RED_LED_PIN, LOW);
+  noTone(BUZZER_PIN); // para o som do buzzer
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  if (lightValue < 755) {
+    lcd.print("LUZ: BOA");
   } else {
-    // Luminosidade baixa - liga led verde e desliga os outros
-    digitalWrite(GREEN_LED_PIN, HIGH);
-    digitalWrite(YELLOW_LED_PIN, LOW);
-    digitalWrite(RED_LED_PIN, LOW);
-    noTone(BUZZER_PIN);   // para o som do buzzer
-    
-    //lcd.clear();
-    //lcd.setCursor(0, 0);
-    //lcd.print("LUZ e TEMP:");
-    //delay(2000);
+    lcd.print("LUZ: BOA");
   }
-  //lcd.clear();
-  lcd.setCursor(0, 0); 
+    lcd.setCursor(0, 1);
+  if (temperaturaC < 16) {
+    lcd.print("TEMP: BOA");
+  } else if (temperaturaC > 25) {
+    lcd.print("TEMP: ALTA");
+  } else {
+    lcd.print("TEMP: MEDIA");
+  }
+    delay(2000);
+}
+  lcd.setCursor(0, 1);
   lcd.print("TEMP: "); // imprime o texto "Temperatura:"
   lcd.print(temperaturaC);
   lcd.print("C");
-  lcd.setCursor(0, 1); 
+  lcd.setCursor(0, 0);
   lcd.print("LUZ: ");
   lcd.print(lightValue);
-  lcd.print(" UV  ");
-
+  delay(2000);
   // imprime valor lido pelo arduino (0 a 1023)
+  tensao_ms = (lightValue * 5.0) / 1024.0;
   Serial.print("Leitura: ");
   Serial.print(lightValue);
-  Serial.print("\t"); // tabulacao     
-
-  Serial.print("Temperatura atual (°C): ");  // Printando em graus Celsius
-  Serial.println(temperaturaC);
+  Serial.print("\t"); // tabulacao
+  Serial.print("Temperatura atual (°C): "); // Printando em graus Celsius
+  Serial.print(temperaturaC);
+  Serial.print("\t");
+  Serial.print("Voltagem: ");
+  Serial.print(tensao_ms);
+  Serial.println("V");
   delay(1000);
 }
